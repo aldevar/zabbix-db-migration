2d04d86607c0dec05d1f68626320065a42df2465
romain <romain.dusson@gmail.com>
2020-06-08 22:58:07 +0200

    Add html flask template and error

--- app.py
+++ app.py
@@ -11,23 +11,27 @@ def index():
 
 @app.route("/migrate", methods=["POST"])
 def migrate():
-    r = request.form
-    zabbix.srczapi = zabbix.ZabbixAPI(r["srcurl"])
-    zabbix.srczapi.login(r["srcuser"], r["srcpass"])
+    try:
+        r = request.form
+        zabbix.srczapi = zabbix.ZabbixAPI(r["srcurl"])
+        zabbix.srczapi.login(r["srcuser"], r["srcpass"])
 
-    zabbix.dstzapi = zabbix.ZabbixAPI(r["dsturl"])
-    zabbix.dstzapi.login(r["dstuser"], r["dstpass"])
+        zabbix.dstzapi = zabbix.ZabbixAPI(r["dsturl"])
+        zabbix.dstzapi.login(r["dstuser"], r["dstpass"])
 
-    zabbix.clean_destination()
-    zabbix.hostgroups_export()
-    zabbix.hostgroups_import()
-    zabbix.proxies_export_import()
-    zabbix.configuration_import(zabbix.configuration_export())
-    zabbix.usergroups_export()
-    zabbix.usergroups_import()
+        zabbix.clean_destination()
+        zabbix.hostgroups_export()
+        zabbix.hostgroups_import()
+        zabbix.proxies_export_import()
+        zabbix.configuration_import(zabbix.configuration_export())
+        zabbix.usergroups_export()
+        zabbix.usergroups_import()
+        return render_template("done.html")
 
+    except:
+        return render_template("error.html")
 
-    return "DONE"
+    
 
 def _getIpAddress():
     return socket.gethostbyname(socket.gethostname())
